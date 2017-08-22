db3490a55cf8913c0ea381b9370a062e1aad7cfb
Ethan Bennett <ebennett@orion.polarisproject.org>
2017-08-22 10:30:16 -0400

    added more items w/ confirmed prices. Still need solid pricing for salesforce.

--- functions.js
+++ functions.js
@@ -9,13 +9,22 @@ function calculatesubtotal() {
   var monitor = 0;
 	var keyboard = 0;
 	var mouse = 0;
+	var adobeAcrobat = 0;
+	var adobeCC = 0;
+	var intranet = 0;
+	var deskPhone = 0;
+	var phoneHeadset = 0;
 		if ( $( "#salesforce-col" ).hasClass( "on" ) ) {  salesforce = 99; }
 		if ( $( "#serviceCloud-col" ).hasClass( "on" ) ) { serviceCloud = 29; }
     if ( $( "#monitor" ).hasClass( "on" ) ) { monitor = 150; }
 		if ( $( "#keyboard" ).hasClass( "on" ) ) { keyboard = 15; }
 		if ( $( "#mouse" ).hasClass( "on" ) ) { mouse = 15; }
-
-	extras = salesforce + serviceCloud + monitor + keyboard + mouse;
+		if ( $( "#adobeAcrobat" ).hasClass( "on" ) ) { adobeAcrobat = 55; }
+		if ( $( "#adobeCC" ).hasClass( "on" ) ) { adobeCC = 245; }
+		if ( $( "#intranet" ).hasClass( "on" ) ) { intranet = 36; }
+		if ( $( "#deskPhone" ).hasClass( "on" ) ) { deskPhone = 80; }
+		if ( $( "#phoneHeadset" ).hasClass( "on" ) ) { phoneHeadset = 100; }
+	extras = salesforce + serviceCloud + monitor + keyboard + mouse + adobeAcrobat + adobeCC + intranet + deskPhone + phoneHeadset;
 	var subtotal = (computers + extras);
 	document.getElementById("total").innerHTML = "$" + subtotal.toFixed(2);
 }
@@ -61,6 +70,56 @@ $('#keyboard').on('click', function() {
 	calculatesubtotal();
 })
 $('#mouse').on('click', function() {
+	var $$ = $(this)
+	if (!$$.is('.on')) {
+		$$.addClass('on');
+		// run function to remove all other 'ons' from other oftens
+	} else {
+		$$.removeClass('on');
+	}
+	calculatesubtotal();
+})
+$('#adobeAcrobat').on('click', function() {
+	var $$ = $(this)
+	if (!$$.is('.on')) {
+		$$.addClass('on');
+		// run function to remove all other 'ons' from other oftens
+	} else {
+		$$.removeClass('on');
+	}
+	calculatesubtotal();
+})
+$('#adobeCC').on('click', function() {
+	var $$ = $(this)
+	if (!$$.is('.on')) {
+		$$.addClass('on');
+		// run function to remove all other 'ons' from other oftens
+	} else {
+		$$.removeClass('on');
+	}
+	calculatesubtotal();
+})
+$('#intranet').on('click', function() {
+	var $$ = $(this)
+	if (!$$.is('.on')) {
+		$$.addClass('on');
+		// run function to remove all other 'ons' from other oftens
+	} else {
+		$$.removeClass('on');
+	}
+	calculatesubtotal();
+})
+$('#deskPhone').on('click', function() {
+	var $$ = $(this)
+	if (!$$.is('.on')) {
+		$$.addClass('on');
+		// run function to remove all other 'ons' from other oftens
+	} else {
+		$$.removeClass('on');
+	}
+	calculatesubtotal();
+})
+$('#phoneHeadset').on('click', function() {
 	var $$ = $(this)
 	if (!$$.is('.on')) {
 		$$.addClass('on');
